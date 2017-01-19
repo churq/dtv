04180306186a4674880c248eda65e2b5ed26d0a4
Rachel Chu <churac0@saig.frd.global>
2017-01-20 02:47:08 +1100

    script to create table in sqlite3

--- db_setup.py
+++ db_setup.py
@@ -1,23 +1,46 @@
 import logging
 import sqlite3
+import log
+from constants import COMPANY_CODE, LOGIN_SOURCE_CODE
 
 from configuration.settings import COMPANY_TABLENAME, SOURCE_TABLENAME
 
+log.init()
 logger = logging.getLogger('DATABASE')
 
-conn = sqlite3.connect('test.db')
+
 logger.info("Opened database successfully")
 
-sql = '''SELECT name FROM sqlite_master WHERE type='table' AND name={};'''.format(COMPANY_TABLENAME)
-if not conn.execute(sql):
-    conn.execute('''CREATE TABLE {}
-           (ID INT PRIMARY KEY     NOT NULL,
-           NAME           TEXT    NOT NULL)'''.format(COMPANY_TABLENAME))
+def db_setup(tablename, code):
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+    def create_table():
+        cur.execute('''CREATE TABLE {}
+                       (ID TEXT PRIMARY KEY     NOT NULL,
+                       NAME TEXT UNIQUE NOT NULL)'''.format(tablename))
+        for item in code:
+            cur.execute("INSERT INTO {}(ID, NAME) VALUES (?,?);".format(tablename), item)
+        logger.info("Table created successfully")
 
-sql = '''SELECT name FROM sqlite_master WHERE type='table' AND name={};'''.format(SOURCE_TABLENAME)
-if not conn.execute(sql):
-    conn.execute('''CREATE TABLE IF NOT EXISTS {}
-           (ID INT PRIMARY KEY     NOT NULL,
-           NAME           TEXT    NOT NULL)'''.format(SOURCE_TABLENAME))
+    cur.execute('DROP TABLE IF EXISTS {}'.format(tablename))
+    create_table()
+    conn.commit()
+    cur.close()
+    conn.close()
 
-print "Table created successfully";
+def acquire_code(table_name, name):
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+    sql = '''select id from {} where name='{}';'''.format(table_name, name)
+    response = cur.execute(sql)
+    code = response.fetchall()
+    cur.close()
+    conn.close()
+
+    return code[0][0] if code else None
+
+if __name__ == '__main__':
+    logger.info('setup database...')
+    db_setup(SOURCE_TABLENAME, LOGIN_SOURCE_CODE)
+    db_setup(COMPANY_TABLENAME, COMPANY_CODE)
+    logger.info('Finish')
