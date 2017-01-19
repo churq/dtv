c441b7ae085b4e552356a3a22c248dd42f7d3f8a
Rachel Chu <churac0@saig.frd.global>
2017-01-20 02:48:16 +1100

    remove unnecessary settings

--- configuration/settings.py
+++ configuration/settings.py
@@ -1,5 +1,4 @@
 import os
-import yaml
 
 DEBUG = True
 
@@ -7,18 +6,5 @@ CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(CURR_DIR)
 LOG_DIR = '{}/logs'.format(BASE_DIR)
 
-CONFIG_FILE = '{}/{}'.format(
-    CURR_DIR,
-    'dev.yml' if DEBUG else 'prod.yml'
-)
-
-
-with open(CONFIG_FILE, 'r') as f:
-    config = yaml.load(f)
-
-SERVER = config['server']
-USER = config['user_name']
-PASSWORD = config['password']
-PORT = config['port']
 COMPANY_TABLENAME = 'company'
 SOURCE_TABLENAME = 'login_source'
